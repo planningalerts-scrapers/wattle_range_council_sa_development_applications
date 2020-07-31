d20ca84ed2e64346f8e5a1a8de13da52dc8da445
MichaelBone <Michael.Bone@senet.com.au>
2020-07-31 16:52:56 +0930

    Removed all rotation.

--- scraper.ts
+++ scraper.ts
@@ -653,13 +653,13 @@ async function parsePdf(url: string) {
         if (page.rotate !== 0)  // degrees
             console.log(`Page is rotated ${page.rotate}°.`);
 
-//        if (page.rotate === 90) {  // degrees
-            for (let cell of cells)
-                rotate90Clockwise(cell);
-            for (let element of elements) {
-                // rotate90Clockwise(element);
-                [ element.y, element.width, element.height ] = [ element.y - element.width, element.height, element.width ];  // artificial adjustment (based on experimentation)
-            }
+// //        if (page.rotate === 90) {  // degrees
+//             for (let cell of cells)
+//                 rotate90Clockwise(cell);
+//             for (let element of elements) {
+//                 rotate90Clockwise(element);
+                // [ element.y, element.width, element.height ] = [ element.y - element.width, element.height, element.width ];  // artificial adjustment (based on experimentation)
+//            }
 //        }
 
 for (let cell of cells)
